668997a93db8798062573d74fb7daa75e4a86e34
ChengleHuang <chuang@jsu.edu>
2014-09-16 14:33:37 -0500

    Update Hangman.java
    change the code

--- Hangman/src/main/java/edu/jsu/mcis/Hangman.java
+++ Hangman/src/main/java/edu/jsu/mcis/Hangman.java
@@ -1,15 +1,17 @@
 package edu.jsu.mcis;
 
 import java.util.*;
+import java.util.Scanner;
 
 public class Hangman {
     public enum Result { WIN, LOSE, NONE };
     private String word;
     private List<Character> usedLetters;
-    
+    private int counter;
     public Hangman() {
         word = "";
         usedLetters = new ArrayList<Character>();
+        counter=0;
     }
     
     public void setWord(String word) {
@@ -17,15 +19,32 @@ public class Hangman {
     }
     
     public boolean available(char c) {
-        return true;
+    	for(i=0; i<word.length){
+    		if(c==word.charAt(i)){
+    			counter++;
+    			return true;
+    		}
+    			
+    	}
+    	return false;
     }
     
     public int guess(char c) {
-        return 2;
+    	if(available(C))
+    		return 2;
+    	else
+    		return 1;
     }
     
     public Result getResult() {
-        return Result.NONE;
+    	if (counter>=word.length());
+    		return WIN;
+    	else{
+    	if (usedLetters>=10)
+    		return LOSE
+    	else
+    		return Result.NONE;
+    	}
     }
     
 }
